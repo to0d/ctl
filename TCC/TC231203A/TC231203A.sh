8eeb4b7e6312a76ba056e0a5724787a7362615ff
todd <to0d@outlook.com>
2023-12-03 11:57:34 +0800

    up

--- TCC/TC231203A/TC231203A.sh
+++ TCC/TC231203A/TC231203A.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
 opt -load ./mypass.so -hello hello.bc -o /dev/null
-opt -load ./mypass.so -hello2 hello.bc -o /dev/null
+opt -load ./mypass.so -hello2 hello.bc -o /dev/null -time-passes
 echo "output: done!"
