0e24074b1824f963a1904c2d179f738a722a6966
to0d <to0d@outlook.com>
2024-02-29 11:15:23 +0800

    up

--- TCC/TC231211B/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int add2(int* __restrict a, int* __restrict b) 
-{
-    *a = 1;
-    *b = 2;
-    return *a + *b ;
-}
-
--- TCC/TC231220B/hello.c
+++
@@ -1,8 +0,0 @@
-int foo(int x) {
-    int y;
-    if (x > 0) {
-        y = 10;
-    }
-    return y;
-}
-
