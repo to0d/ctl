76b618a978b9db90ca303c46e74daed703c41e0a
liuyan <to0d@outlook.com>
2024-07-08 16:58:57 +0800

    up

--- TCC/TC240708E/TC240708E.sh
+++ TCC/TC240708E/TC240708E.sh
@@ -3,10 +3,20 @@
 /usr/lib/llvm-14/bin/clang -c test.c -S -emit-llvm -o test_14.ll
 echo ""
 
+/usr/lib/llvm-14/bin/clang -c test.c -S -emit-llvm -o test_14_2.ll -Xclang -no-opaque-pointers
+echo ""
+
+
 /usr/lib/llvm-17/bin/clang -c test.c -S -emit-llvm -o test_17.ll
 echo ""
 
-/usr/lib/llvm-17/bin/clang -c test.c -S -emit-llvm -o output.ll --no-opaque-pointer
+/usr/lib/llvm-17/bin/clang -c test.c -S -emit-llvm -o output.ll -Xclang -no-opaque-pointers
+echo ""
+
+/usr/lib/llvm-15/bin/clang -c test.c -S -emit-llvm -o output.ll -Xclang -no-opaque-pointers
+echo ""
+
+/usr/lib/llvm-15/bin/opt test_17.ll -S -o output.ll --opaque-pointers
 echo ""
 
 echo "output: done!"
